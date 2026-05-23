57723d9eb0e4af5cb3cbc41ec37fe7f38710d7dd
JackBell-p <3452896431@qq.com>
2026-05-23 19:09:41 +0800

    move window_setru.rs file to graphics folder

--- src/plugins/graphics.rs
+++ src/plugins/graphics.rs
@@ -2,11 +2,13 @@ use bevy::prelude::*;
 
 mod camera2d;
 mod mesh;
+mod window_setup;
 
 pub struct Graphics;
 
 impl Plugin for Graphics {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, (mesh::setup, camera2d::setup_camera2d));
+        app.add_plugins(window_setup::WindowSetup)
+            .add_systems(Startup, (mesh::setup, camera2d::setup_camera2d));
     }
 }
